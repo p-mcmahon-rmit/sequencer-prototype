4febd89059d6a68bb8ec938a45a9e7cac8a41eb6
Patrick McMahon <patrick.mcmahon@rmit.edu.au>
2026-04-02 13:33:41 +1100

    examples for class

--- sequencer.js
+++ sequencer.js
@@ -1,17 +1,36 @@
-/* find modal */
-let introModal = document.getElementById("introDialog");
+/* find intro modal */
+const introModal = document.getElementById("introDialog");
 /* to get the backdrop working we need to open the modal with js */
-document.getElementById("introDialog").showModal();
+introModal.showModal();
 /* find modal close button and add an eventlistener */
-document.getElementById("dialogCloseButton").addEventListener("click", () => {
+document.getElementById("introDialogCloseButton").addEventListener("click", () => {
   introModal.close();
 });
 /* finally we want to initialize the synthesizer when the modal is closed */
 /* because this can be through the above button, or by pressing esc, we tie it to the actual close event */
 /* the referenced toneInit function is defined in toneSetup.js */
 introModal.addEventListener("close", () => {
+  // open info modal on intro close if it exists
+  if(infoModal){
+    infoModal.showModal();
+  }
   synth.chain(Tone.Destination);
 });
+
+// find info modal
+const infoModal = document.getElementById("infoDialog");
+// because i'm demonstrating adding this to a specific page, i'm encapsulating the logic in an if
+if(infoModal){
+
+  document.getElementById("infoDialogOpenButton").addEventListener("click", () => {
+    infoModal.showModal();
+  });
+
+  document.getElementById("infoDialogCloseButton").addEventListener("click", () => {
+    infoModal.close();
+  });
+
+}
 
 /* basic synth for note input */
 
@@ -69,6 +88,19 @@ let stepBoxes = Array.from(document.getElementsByClassName("seqStepBox"));
 
 let stepInputs = Array.from(document.getElementsByClassName("seqStep"));
 
+// create default settings
+let initNotes = [
+    0,
+    null,
+    4,
+    "reset",
+    null,
+    0,
+    "reset",
+    0
+];
+let setDefaults = true
+
 stepInputs.forEach((stepInput, index) => {
 
   let pitchInput = stepInput.nextElementSibling;
@@ -102,12 +134,18 @@ stepInputs.forEach((stepInput, index) => {
     } else if(!stepInput.checked) {
       stepInput.readOnly = stepInput.indeterminate = true;
       currentSequence[index].active = false;
+      // change label
+      stepInput.parentElement.lastElementChild.innerText = "off";
       if (index !== 0) {
         currentSequence[index-1].reset = true;
+        // change label
+        stepInput.parentElement.lastElementChild.innerText = "reset";
       }
       currentSequence[index].active = false;
     } else {
       currentSequence[index].active = true;
+      // change label
+      stepInput.parentElement.lastElementChild.innerText = currentSequence[index].note;
     }
   });
   
@@ -115,14 +153,29 @@ stepInputs.forEach((stepInput, index) => {
     let newValue = e.target.value;
     let quotient = Math.floor(newValue/12);
     let remainder = newValue % 12;
-    let newNoteValue; 
+    let newNoteValue;
     if(remainder >= 0){
       newNoteValue = `${noteNames[remainder]}${3 + quotient}`;
     } else {
       newNoteValue = `${noteNames[noteNames.length + remainder]}${3 + quotient}`;
     }
+    // if active change label
+    if(currentSequence[index].active){
+      pitchInput.nextElementSibling.textContent = newNoteValue;
+    }
+    // update actual note value
     currentSequence[index].note = newNoteValue;
   })
+
+  // only run if defaults present
+  if(setDefaults){
+    if(initNotes[index] === "reset"){
+      //stepInput.click();
+    } else if (typeof initNotes[index] === 'number'){
+      pitchInput.value = initNotes[index];
+      stepInput.click();
+    }
+  }
 });
 
 function nextStep(time){
